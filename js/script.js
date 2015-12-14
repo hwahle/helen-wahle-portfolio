4ac1b789c2744bad257930a61a4600cc38b910e7
hwahle <helenwahle@gmail.com>
2015-12-13 18:27:16 -0800

    animated modal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // add our javascript here
+$(document).ready( function() {
+	$('.img-holder').imageScroll({
+		holderMaxHeight: 600
+	})
+
+	$('.portfolio a').animatedModal();
+})
