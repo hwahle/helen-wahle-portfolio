4e47e99403a54da6e1d20155a60371ef336cd785
saliya seneviratne <sseneviratne@thinkful.com>
2015-12-15 09:52:43 -0500

    working modals

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
+$(document).ready(function() {
+    $('.img-holder').imageScroll({
+        holderMaxHeight: 600
+    })
 
+    // 1st modal does not need any options
+    $("#portfolio01").animatedModal();
 
+    // 2nd modal on must specify modalTarget!
+    $("#portfolio02").animatedModal({
+        modalTarget: 'modal-02'
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// add our javascript here
-$(document).ready( function() {
-	$('.img-holder').imageScroll({
-		holderMaxHeight: 600
-	})
-
-	$('.portfolio a').animatedModal();
+    // fancier example
+    $("#portfolio03").animatedModal({
+        modalTarget: 'modal-03',
+        animatedIn: 'lightSpeedIn',
+        animatedOut: 'bounceOutDown',
+        color: '#3498db',
+        // Callbacks
+        beforeOpen: function() {
+            console.log("The animation was called");
+        },
+        afterOpen: function() {
+            console.log("The animation is completed");
+        },
+        beforeClose: function() {
+            console.log("The animation was called");
+        },
+        afterClose: function() {
+            console.log("The animation is completed");
+        }
+    });
 })
