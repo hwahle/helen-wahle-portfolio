1ce89f38cd8074c49243ccb99e8bf4c64e9b8956
hwahle <helenwahle@gmail.com>
2016-02-16 16:27:14 -0800

    added 4th modal - instagram api

--- js/script.js
+++ js/script.js
@@ -11,12 +11,33 @@ $(document).ready(function() {
         modalTarget: 'modal-02'
     });
 
+
     // fancier example
     $("#portfolio03").animatedModal({
         modalTarget: 'modal-03',
         animatedIn: 'lightSpeedIn',
         animatedOut: 'bounceOutDown',
         color: '#3498db',
+        // Callbacks
+        beforeOpen: function() {
+            console.log("The animation was called");
+        },
+        afterOpen: function() {
+            console.log("The animation is completed");
+        },
+        beforeClose: function() {
+            console.log("The animation was called");
+        },
+        afterClose: function() {
+            console.log("The animation is completed");
+        }
+    });
+
+     $("#portfolio04").animatedModal({
+        modalTarget: 'modal-04',
+        animatedIn: 'fadeInUp',
+        animatedOut: 'fadeOutUp',
+        color: '#6633CC',
         // Callbacks
         beforeOpen: function() {
             console.log("The animation was called");
